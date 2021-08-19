4d7eacf7b98b190c86680954f6f32f24141e76f8
kovacsv <viktorkovacs@gmail.com>
2021-08-19 14:46:04 +0200

    Remove treat warning as errors.

--- assimpjs/src/main.cpp
+++ assimpjs/src/main.cpp
@@ -1,9 +1,23 @@
 #include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 int MeaningOfLife ()
 {
 	Assimp::Importer importer;
 	return 42;
+}
+
+bool ImportFile (const std::string& filePath)
+{
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile (filePath,
+		aiProcess_CalcTangentSpace |
+		aiProcess_Triangulate |
+		aiProcess_JoinIdenticalVertices |
+		aiProcess_SortByPType);
+
+	return true;
 }
 
 #ifdef EMSCRIPTEN
