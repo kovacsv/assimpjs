58466d3d855c68eefbdea388a59af4b724f68232
kovacsv <viktorkovacs@gmail.com>
2021-08-20 18:02:46 +0200

    Update test.

--- assimpjs/test/node_test.js
+++ assimpjs/test/node_test.js
@@ -3,8 +3,8 @@ let fs = require ('fs');
 const assimpjs = require ('./assimpjs.js')();
 assimpjs.then ((ajs) => {
 	let fileList = new ajs.FileList ();
-	fileList.SetPrimaryFile ('cube_with_materials.obj', fs.readFileSync ('testfiles/cube_with_materials.obj'))
-	fileList.AddSecondaryFile ('cube_with_materials.mtl', fs.readFileSync ('testfiles/cube_with_materials.mtl'))
+	fileList.SetPrimaryFile ('cube_with_materials.obj', fs.readFileSync ('testfiles/cube_with_materials.obj'));
+	fileList.AddSecondaryFile ('cube_with_materials.mtl', fs.readFileSync ('testfiles/cube_with_materials.mtl'));
 	let result = ajs.ImportFile (fileList);
 	let resultJson = JSON.parse (result);
 	console.log (resultJson);
