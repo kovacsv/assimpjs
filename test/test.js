f7ab1dfc7a21b76dedd2964c72e37fd11a3f7b78
kovacsv <viktorkovacs@gmail.com>
2021-08-21 09:37:02 +0200

    Add basic mocha test.

---
+++ test/test.js
@@ -0,0 +1,39 @@
+var fs = require ('fs');
+var assert = require ('assert');
+var assimpjs = require ('../em_build/Debug/assimpjs.js')();
+
+var ajs = null;
+before (async function () {
+	ajs = await assimpjs;
+});
+
+function LoadModel (files)
+{
+	let fileList = new ajs.FileList ();
+	for (let i = 0; i < files.length; i++) {
+		fileList.AddFile (files[i], fs.readFileSync (files[i]))
+	}
+	return ajs.ImportModel (fileList);
+}
+
+function IsError (files)
+{
+	return LoadModel (files) === "error";
+}
+
+function IsSuccess (files)
+{
+	return LoadModel (files) !== "error";
+}
+
+describe ('Importer', function () {
+
+it ('Error', function () {
+	assert (IsError ([]));
+});
+
+it ('3DS', function () {
+	assert (IsSuccess (['../assimp/test/models/3DS/cubes_with_alpha.3DS']));
+});
+
+});
