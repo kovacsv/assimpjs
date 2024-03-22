81583315510c1e275715391bd9d321dcf46d1805
Hannes <hannes-s.c@hotmail.de>
2024-03-22 21:57:08 +0100

    Update for windows builds
    Uses the latest emsdk now

--- tools/setup_emscripten_win.bat
+++ tools/setup_emscripten_win.bat
@@ -2,8 +2,10 @@ pushd %~dp0\..
 
 call git clone https://github.com/emscripten-core/emsdk.git
 call cd emsdk
-call emsdk install 3.1.35
-call emsdk activate 3.1.35
+::call emsdk install 3.1.35
+call emsdk install latest
+::call emsdk activate 3.1.35
+call emsdk activate latest
 call emsdk install mingw-7.1.0-64bit
 call emsdk activate mingw-7.1.0-64bit
 call cd ..
